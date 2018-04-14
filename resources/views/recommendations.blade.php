b6f3107d7fbb06beb4ee0b04d2687222f0da97a1
Dan Mopsick <Dan.Mopsick@ibm.com@Dans-MacBook-Pro.local>
2018-04-14 19:24:14 -0400

    Made somewhat progess for Watson lmao not really but I tried and I love you.

--- resources/views/recommendations.blade.php
+++ resources/views/recommendations.blade.php
@@ -8,9 +8,13 @@
           <input class="recommendations-search-field" id="question" type="text" placeholder="Where can upperclassman live?">
           <label for="question">Ask a question</label>
           <div class="recommendations-searchbutton">
-            <a class="waves-effect waves-light btn recommendations-search-button red"> <i class="material-icons">question_answer</i></a>
+            <a onClick="classifyText()" class="waves-effect waves-light btn recommendations-search-button red"> <i class="material-icons">question_answer</i></a>
           </div>
     </div>
   </div>
 
+@endsection
+
+@section('js')
+  <script src="/js/recommendations.js"></script>
 @endsection
