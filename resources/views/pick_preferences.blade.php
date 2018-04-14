c195bde04d2c8c902375057552e8bf3ce0637ab1
Thomas Magnusson <thomas.magnusson1@marist.edu>
2018-04-14 19:06:54 -0400

    preferences frontend

--- resources/views/pick_preferences.blade.php
+++ resources/views/pick_preferences.blade.php
@@ -2,57 +2,58 @@
 
 @section('content')
 
+  @php
+  $residenceAreas = \App\Models\ResidenceArea::all();
+@endphp
 
+<form method="POST"id="top-3" onsubmit="onSubPrefs(event)">
+  {{ csrf_field() }}
 
-  <form method="POST"id="top-3">
-    {{ csrf_field() }}
-
-    <div class="row">
-      <div class="col m6">
-        <h3>
-          Click and Drag your top 3 Preferences
-        </h3>
-        <div class="prefs">
-          <div class="pref">
-            <h5>First Preference</h5>
-            <br>
-            <div data-order="1" id="pref-1" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" class="card-panel ra-drop grey-text text-lighten-1">
-            </div>
-          </div>
-          <div class="pref">
-            <h5>Second Preference</h5>
-            <br>
-            <div data-order="2" id="pref-2" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" class="card-panel ra-drop grey-text text-lighten-1">
-            </div>
-          </div>
-          <div class="pref">
-            <h5>Third Preference</h5>
-            <br>
-            <div data-order="3" id="pref-3" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" class="card-panel ra-drop grey-text text-lighten-1">
-            </div>
+  <div class="row">
+    <div class="col m6">
+      <h3>
+        Click and Drag your top 3 Preferences
+      </h3>
+      <div class="prefs">
+        <div class="pref">
+          <h5>First Preference</h5>
+          <br>
+          <div data-order="1" id="pref-1" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" class="card-panel ra-drop grey-text text-lighten-1">
           </div>
         </div>
-
+        <div class="pref">
+          <h5>Second Preference</h5>
+          <br>
+          <div data-order="2" id="pref-2" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" class="card-panel ra-drop grey-text text-lighten-1">
+          </div>
+        </div>
+        <div class="pref">
+          <h5>Third Preference</h5>
+          <br>
+          <div data-order="3" id="pref-3" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" class="card-panel ra-drop grey-text text-lighten-1">
+          </div>
+        </div>
       </div>
-      <div class="col m6">
-        <h3>
-          Residence Areas
-        </h3>
-        <div class="card-panel ra" ondrop="drop_handler(event);" ondragover="dragover_handler(event);">
-          <div id="sheahan" draggable="true" ondragstart="dragstart_handler(event);" class="card-panel ra-card">
-            Sheahan
+      <input id="submitPrefsButton" class="btn red" type="submit" value="Submit Preferences" disabled>
+    </div>
+    <div class="col m6">
+      <h3>
+        Residence Areas
+      </h3>
+
+      <div class="card-panel ra" ondrop="drop_handler(event);" ondragover="dragover_handler(event);">
+        @foreach ($residenceAreas as $ra)
+          <div id="{{ $ra->name }}" onclick="touchDeviceSelect(this)" draggable="true" ondragstart="dragstart_handler(event);" class="card-panel ra-card">
+            <img draggable="false" src="{{ $ra->image_url }}">
+            <p>{{ $ra->name }}</p>
           </div>
-          <div id="leo" draggable="true" ondragstart="dragstart_handler(event);" class="card-panel ra-card">
-            Leo
-          </div>
-          <div id="champ" draggable="true" ondragstart="dragstart_handler(event);" class="card-panel ra-card">
-            Champ
-          </div>
-        </div>
+        @endforeach
       </div>
     </div>
 
-  </form>
+  </div>
+
+</form>
 
 
 @endsection
