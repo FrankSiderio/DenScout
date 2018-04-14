d2b9b93e3fffbba026fb242d7d7b72a9541b1ed0
Dan Mopsick <Dan.Mopsick@ibm.com@Dans-MacBook-Pro.local>
2018-04-14 18:23:22 -0400

    Added some booty css and made my stupid css less stupid and ugly.

--- resources/views/recommendations.blade.php
+++ resources/views/recommendations.blade.php
@@ -4,14 +4,13 @@
 
   <h2>Housing Recommendations</h2> 
   <div class="row">
-    <div class="input-field col s12 recommendations-search">
+    <div class="input-field col s10 recommendations-search">
           <input class="recommendations-search-field" id="question" type="text" placeholder="Where can upperclassman live?">
           <label for="question">Ask a question</label>
           <div class="recommendations-searchbutton">
-            <a class="waves-effect waves-light btn recommendations-search-button"> <i class="material-icons">question_answer</i></a>
+            <a class="waves-effect waves-light btn recommendations-search-button red"> <i class="material-icons">question_answer</i></a>
           </div>
     </div>
   </div>
-
 
 @endsection
