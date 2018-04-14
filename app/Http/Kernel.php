20f469aba06e0106c18be87fd495f22917956b2b
Frank Siderio <frank.siderio1@marist.edu>
2018-04-13 22:30:11 -0400

    configuring cas

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,10 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
         ],
+        
+        'CasAuth' => [
+          \App\Http\Middleware\CasAuth::class,
+        ],
     ];
 
     /**
